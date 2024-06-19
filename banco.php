57009c153d86a916aeb859b9bd6def5a13d854f9
danfasst <danielcfcunha2005@gmail.com>
2024-06-19 09:44:52 -0300

    style css

--- banco.php
+++ banco.php
@@ -1,13 +1,58 @@
+<pre>
 <?php
-    $banco = new mysqli("localhost", "root", "", "system_db");
 
-    $busca = $banco->query("SELECT * FROM usuario");
-    echo var_dump($busca);
-    echo print_r($busca);
+$banco = new mysqli("localhost:3307", "root", "", "system_db");
 
-    echo "<br>----------------------------<br>";
-    $obj = $busca->fetch_object();
-    echo print_r($obj);
+function cadastarFilme($titulo, $autor, $sinopse, $nota): void
+{
+    global $banco;
+
+    $add = "INSERT INTO filmes (titulo, autor, sinopse, nota) VALUES ('$titulo', '$autor', '$sinopse', '$nota')";
+
+    $banco->query($add);
+}
+
+if (isset($_POST['submit'])) {
+    $titulo = $_POST['titulo'];
+    $autor = $_POST['autor'];
+    $sinopse = $_POST['sinopse'];
+    $nota = $_POST['nota'];
+
+    if (empty($titulo) || empty($autor) || empty($sinopse) || empty($nota)) {
+        echo "<p>Algum campo está vazio!</p>";
+    } else {
+
+        cadastarFilme($titulo, $autor, $sinopse, $nota);
+    }
+}
+
+if (isset($_POST['removeAll'])) {
+    $removerAll = $banco->query("DELETE FROM filmes WHERE ID != 0");
+}
+
+if (isset($_POST['remove'])) {
+    $id = $_POST['id'];
+    if ($id > 0) {
+        $remover = $banco->query("DELETE FROM filmes WHERE ID = $id");
+    }
+}
+
+$buscaAll = $banco->query("SELECT * FROM filmes");
+
+
+echo "<ul>";
+while ($obj = $buscaAll->fetch_object()) {
+
+    echo "<form class='formFilme' method='post'>";
+    echo "<br><li>Titulo: " . $obj->titulo . " <br> Autor: " . $obj->autor . " <br> Sinopse: " . $obj->sinopse . " <br> Nota: " . $obj->nota . "</li>";
+    echo  "<input type='hidden' name='id' value='$obj->ID'>
+        <input type='submit' value='Remover' name='remove'>
+    </form>";
+}
+echo "</ul>";
+
+
 
 
 ?>
+</pre>
--- index.php
+++ index.php
@@ -1,13 +1,72 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Filmes PHP</title>
+    <style>
+        body {
+            background-color: #454545;
+            margin: 0;
+        }
+
+        .formMain {
+            display: flex;
+            flex-direction: column;
+            align-items: start;
+            gap: 20px;
+            margin: 40px 0 0 30px;
+        }
+
+        .formMain>input {
+            padding: 8px;
+            border-radius: 10px;
+        }
+
+        ul {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 8px;
+            padding: 0;
+            list-style: none;
+            margin: 5px 10px 5px;
+        }
+
+        li {
+            color: white;
+            background-color: #581845;
+            width: 200px;
+            height: 250px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 10px;
+            overflow: hidden;
+        }
+
+        .titulo {
+            font-size: 10px;
+        }
+    </style>
 </head>
+
 <body>
 
-<?php
-    session_start();
+    <form class="formMain" method="post">
+        <input placeholder="Titulo" type="text" name="titulo">
+        <input placeholder="Autor" type="text" name="autor">
+        <input id="sinopse" placeholder="Sinopse" type="text" name="sinopse">
+        <input placeholder="Nota" type="text" name="nota">
+        <input type="submit" value="Enviar" name="submit">
+        <input type="submit" value="Apagar Tudo" name="removeAll">
+    </form>
+
+    <?php
     require_once "banco.php";
-?>
+
+    ?>
+
+</body>
+
+</html>
